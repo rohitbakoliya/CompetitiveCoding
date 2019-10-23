7204e5508522509f3743b7dd4c4fd0401a066975
ROHIT BAKOLIYA <46809038+rohitbakoliya@users.noreply.github.com>
2019-10-23 20:01:46 +0530

    Create Special String.cpp

---
+++ Hackerearth/Special String.cpp
@@ -0,0 +1,3 @@
+//   https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/special-substrings-9fb5dbe8/
+
+//try to solve it...it is nice problem
